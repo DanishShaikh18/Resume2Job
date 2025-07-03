3accb80d6005b1295a1664c354cac0b14151158b
Danish_Shaikh <danish89761@gmail.com>
2025-07-03 19:35:12 +0530

    Extraction Code Completed

--- Extraction/extractor.py
+++ Extraction/extractor.py
@@ -3,8 +3,10 @@ import fitz
 from docx import Document
 import easyocr
 import magic
+from processing.cleaner import clean_text
 
-file_path = r'doc\Danish-Shaikh-DataAnalyst.pdf'
+
+file_path = r'doc\Danish-Shaikh-Customer-Support-Cactus.pdf'
 
 def f_type(path):
     mime = magic.Magic(mime=True)
@@ -15,35 +17,49 @@ file_type = f_type(file_path)
 # Extract text from PDF
 def extract_from_pdf(path):
     doc = fitz.open(path)
+    text = ""
     for page in doc:
-        return page.get_text()
+        text += page.get_text()
+    return text
 
+
+#Extract text from DOCX
 def extract_from_docx(path):
     doc = Document(path)
+    
+    text = "\n".join(
+        para.text.strip() for para in doc.paragraphs if para.text.strip()
+    )
 
+    tables = []
+    for table in doc.tables:
+        for row in table.rows:
+            row_data = [cell.text.strip() for cell in row.cells if cell.text.strip()]
+            if row_data:
+                tables.append(row_data)
+    
+    return [text, tables]
 
-
-
-
-
+reader = easyocr.Reader(['en'])
+#Extract text from Image
 def extract_from_image(path):
-    pass
+    results = reader.readtext(path)
+    lines = [text for _, text, conf in results if conf > 0.5]
+    return "\n".join(lines)
 
 if __name__ == '__main__':
 
-    text = ''
-
     if file_type == 'application/pdf':
-        text = extract_from_pdf(file_path)
+        extracted_text = extract_from_pdf(file_path)
     elif file_type.startswith('image/'):
-        text = extract_from_image(file_path)
+        extracted_text = extract_from_image(file_path)
     elif file_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-        text = extract_from_docx(file_path)
+        extracted_text = extract_from_docx(file_path)
     else:
         print("Unknown or unsupported file type")
 
 
-print(text)
+clean_text(extracted_text)
 
 
 
---
+++ processing/cleaner.py
@@ -0,0 +1,5 @@
+# cleaner.py
+
+def clean_text(text):
+    print("Received text in cleaner.py:")
+    print(text)
